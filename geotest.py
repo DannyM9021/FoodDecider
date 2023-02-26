addf01ee2790172622e5f0464c1dad648cb48ea1
Spencer K Chuong <spencerchuong0@gmail.com>
2023-02-26 11:59:14 -0800

    tried to format

--- geotest.py
+++ geotest.py
@@ -1,12 +1,18 @@
+import os  
+import socket 
+import geocoder
+import requests
+import json
+import time
+import requests
+import json
+import googlemaps
 # import geocoder
 # g = geocoder.geonames('Mountain View, CA', maxRows=5)
 # print(len(g))
 # for result in g:
 #   print(result.address, result.latlng)
 
-import os  
-import socket 
-import geocoder
 
 # # IP = socket.gethostbyname(hostname)
 # # print(IP)
@@ -17,9 +23,13 @@ import geocoder
 # # print(g.latlng)
 # # print(g.city)
 
-import requests
-import json
 
+def IPConverter():
+    my_ip = get()
+    g = geocoder.ip(my_ip)
+    g = geocoder.ip('me')
+    print(g.latlng)
+    return(g.latlng)
 def get():
     endpoint = 'https://ipinfo.io/json'
     response = requests.get(endpoint, verify = True)
@@ -29,31 +39,31 @@ def get():
         exit()
 
     data = response.json()
-
     return data['ip']
 
+IPConverter()
+print(IPConverter()[0])
 #get my ip
-my_ip = get()
+#my_ip = get()
 
 #print my ip
-print(my_ip)
+#print(my_ip)
 
-g = geocoder.ip(my_ip)
-g = geocoder.ip('me')
-print(g.latlng)
-print(g.city)
+#g = geocoder.ip(my_ip)
+#g = geocoder.ip('me')
+#print(g.latlng)
+#print(g.latlng[0])
+#print(g.city)
 
 
 
-import requests
-import json
-import googlemaps
+
 
 APIKEY = "AIzaSyCREubepe3S9zdisc8Lt65VzpNdseuQKmo"
 
-def findPlaces(loc=("33.7939","-118.1192"),radius=4000, pagetoken = None):
+def findPlaces(loc=(IPConverter()[0],IPConverter()[1]),radius=4000, pagetoken = None):
    lat, lng = loc
-   type = "restaurant"
+   type = "meal_delivery"
    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius={radius}&type={type}&key={APIKEY}{pagetoken}".format(lat = lat, lng = lng, radius = radius, type = type,APIKEY = APIKEY, pagetoken = "&pagetoken="+pagetoken if pagetoken else "")
    print(url)
    response = requests.get(url)
@@ -82,9 +92,9 @@ def findPlaces(loc=("33.7939","-118.1192"),radius=4000, pagetoken = None):
 # pagetoken = "CpQFhwIAADQWOcVI1wll-B869Z24El48rXw18gKoab_keD65V18zFEvPjKIfrS79Pc_vXJcZQtOuF0RObQG20ph-GE3ssP3k1fu8zsYbw5g3UPbSjAvQLdXkdD1qAWztXj7hc5Kxc4pYRyGM1_ljVOHg3Py_zSlYscnoNjCvRua2MDQgusCsEquNqGREFdvhjDkbeMhEFYxHucTnIn96OxIJEpamePTHsBooYyPBaa_ejGZ_C99QeDjpSkSKBgEe3aL1uWKlYhsGKh7biQUR5rKsKPodwccLIrW8Gr5tag3NH0sLPExHHvqzlpkj--KIuydTVjPH7u2zHxmPByServ2S5xjXYUBRr-ly3e1xPsVMhZZH9TxfttCIHLscBvpvCswIfaGYdl3bEzsrFISfpp0rpKtlp9gWGY7Tbk2n6s3etCHQEHn2qmM8bsJwkZV81pUWN0j9C9RX-ywOyIKY2yp1w_Iq1mRwOwY4mckbicOoooHiV6JER4xe7Kizw9hbXOnezn_NMk15TLwRoXlfL1s73uwogo-VWE8c-V1HqRpWQSyudRhLwhOEclrICXIdxICOgTgYO1z57xCEerw3QUL_7MPDrlbbh_AlX8I6Jfe8IhQ1Fkqu_njatm6aBTjkp2CSqlvZJpI_Lrv330VcyFEqBkGn7NJew3I9xofSrBaXFa8ABi6DXQm6-yC32OEyf7GHNXINjT1IB0yh6KR6c0qzaqiqOzKcuuai9XqEMQNNKyi6EuhzH5TP9YA56N3JhnXRFhs2aWHZhLlieVI6_uqzpZSgYjUem8aQrMTlmHw0kIYU8I-Ca041C4Zm2gMezwygRrhzsOoAmbmu96nft0KuIWTB3A_xGVKYQ2qjb2KRM7nsglnSEhDoNs8EhvuIm0FQs30YSCp5GhRO3b3Tn5rsLuwiWgu8hwEGhL0S1A"
 pagetoken = None
 
-while True:
+for x in range(1):
+#while True:
      pagetoken = findPlaces(pagetoken=pagetoken)
-     import time
      time.sleep(5)
 
      if not pagetoken:
