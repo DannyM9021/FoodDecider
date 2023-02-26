1de8e2b4209bd6bf64f2c7298ede8508f0786576
DannyM9021 <dmoreno.1109@gmail.com>
2023-02-26 13:49:14 -0800

    Updated

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from GUI import *
 from decider import *
+import random
 def main():
     cond1 = welcome_page()
     results = submission_page(cond1)
     if results is not None:
-        searching(list(results.values()))
-
+        places = searching(list(results.values()))
+        rng = random.randint(0, len(places)-1)
+        print(places[rng][1])
 
 main()
