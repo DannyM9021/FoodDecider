cc029803b2b3aa86b19b9c6f8a6d9181b1350593
Spencer K Chuong <spencerchuong0@gmail.com>
2023-02-26 12:49:42 -0800

    added in searching arguements

--- decider.py
+++ decider.py
@@ -30,7 +30,23 @@ def get():
     data = response.json()
     return data['ip']
 
-def searching():
+def searching(food, money):
+    ipAddress = "@" + str(IPConverter()[0]) + "," + str(IPConverter()[1]) + ",15z"
+    #print(ipAddress)
+    params = {
+    "engine": "google_maps",
+    "q": str(food),
+    "ll": ipAddress,
+    "type": "search",
+    "api_key": "5aa1909e29c5608f5423006db43d6f8332b1ab45dacd27380ae14ebae3ab26a5"
+    } 
+    priceRange = ""
+    if money == "inexpensive":
+        priceRange += "$"
+    elif money == "moderate":
+        priceRange += "$$"
+    else:
+        money += "$$$"
     search = GoogleSearch(params)
     results = search.get_dict()
     local_results = results["local_results"]
@@ -38,22 +54,14 @@ def searching():
     #print(local_results)
     for x in range(len(local_results)-1):
         local_list = []
-        if "title" in local_results[x]:
-            local_list.append(local_results[x]["title"])
         if "price" in local_results[x]:
-            local_list.append(local_results[x]["price"])
-        if "rating" in local_results[x]:
-            local_list.append(local_results[x]["rating"])
-        if "address" in local_results[x]:
-            local_list.append(local_results[x]["address"])
+            if local_results[x]["price"] == priceRange:
+                local_list.append(local_results[x]["price"])
+                if "title" in local_results[x]:
+                    local_list.append(local_results[x]["title"])
+                if "rating" in local_results[x]:
+                    local_list.append(local_results[x]["rating"])
+                if "address" in local_results[x]:
+                    local_list.append(local_results[x]["address"])
         restaurant_list.append(local_list)
     return restaurant_list
-ipAddress = "@" + str(IPConverter()[0]) + "," + str(IPConverter()[1]) + ",15z"
-#print(ipAddress)
-params = {
-  "engine": "google_maps",
-  "q": "ramen",
-  "ll": ipAddress,
-  "type": "search",
-  "api_key": "5aa1909e29c5608f5423006db43d6f8332b1ab45dacd27380ae14ebae3ab26a5"
-} 
