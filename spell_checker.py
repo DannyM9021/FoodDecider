3820c3d23f70f857b02248e86cc45f51253cb40e
DannyM9021 <dmoreno.1109@gmail.com>
2023-02-25 18:16:38 -0800

    Gui looks good

---
+++ spell_checker.py
@@ -0,0 +1,11 @@
+from spellchecker import SpellChecker # https://pyspellchecker.readthedocs.io/en/latest/
+
+def spell_check(place):
+
+    checker = SpellChecker()
+    # find those words that may be misspelled
+    lookup = checker.unknown([place])
+
+    for word in lookup:
+        # Get a list of `likely` options
+        return checker.candidates(word)
